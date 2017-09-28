e8f2cbaae56e83f21988804973993657648ae5c0
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-28 11:50:04 -0700

    Lab finished and Readme updated

---
+++ Lab13Tom/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab13Tom.Controllers
+{
+    [Route("Error")]
+    public class ErrorController
+    {
+        [Route("Error")]
+        public string Index()
+        {
+            return "Oops, there was an error! Please contact the school";
+        }
+    }
+}
