d5772e11507ad6de947ea9428dabda8b4974c3d9
George Gaylor <georgegaylor83@outlook.com>
2017-09-28 13:16:34 -0700

    Initial work

---
+++ Lab13George/Lab13George/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab13George.Controllers
+{
+    [Route("Error")]
+    public class ErrorController : Controller
+    {
+        [Route("Error")]
+        public IActionResult Index() => View();
+    }
+}
