a798d698a6cd5464eb0f9004bf73c52feb8b2111
Austin Cohrs <acohrsdev@outlook.com>
2017-09-28 11:06:34 -0700

    Austin-Lab13

---
+++ Lab13Models/Data/Lab13ModelsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab13Models.Models
+{
+    public class Lab13ModelsContext : DbContext
+    {
+        public Lab13ModelsContext (DbContextOptions<Lab13ModelsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab13Models.Models.StudentModel> StudentModel { get; set; }
+    }
+}
---
+++ Lab13Models/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab13Models.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
