b0c0c05304f05b43bd4d12663d1898e34cc23ea4
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-28 11:34:37 -0700

    Database set up, will finish this weekend. Not feeling too hot

---
+++ lab13_brian/lab13_brian/Data/lab13_brianContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab13_brian.Models
+{
+    public class lab13_brianContext : DbContext
+    {
+        public lab13_brianContext (DbContextOptions<lab13_brianContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab13_brian.Models.Student> Student { get; set; }
+    }
+}
---
+++ lab13_brian/lab13_brian/Migrations/20170928175456_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace lab13_brian.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    ClassType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
