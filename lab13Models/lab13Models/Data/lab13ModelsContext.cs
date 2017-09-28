1c45225a46786e0df2eb8627976b23dd5416781b
Gregory Dukes <kcils360@gmail.com>
2017-09-28 11:55:51 -0700

    completed lab13 models

---
+++ lab13Models/lab13Models/Data/lab13ModelsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab13Models.Models
+{
+    public class lab13ModelsContext : DbContext
+    {
+        public lab13ModelsContext (DbContextOptions<lab13ModelsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab13Models.Models.Students> Students { get; set; }
+    }
+}
---
+++ lab13Models/lab13Models/Models/Students.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab13Models.Models
+{
+    public class Students
+    {
+        public int ID { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+    }
+}
