5ab93ec11587032419b8834cbe806c0033c07072
Cameron Pam <cammpam.creative@gmail.com>
2017-10-01 22:09:33 -0700

    Lab 13 Cameron

---
+++ Lab13Cameron/Lab13Cameron/Data/Lab13CameronContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab13Cameron.Models
+{
+    public class Lab13CameronContext : DbContext
+    {
+        public Lab13CameronContext (DbContextOptions<Lab13CameronContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab13Cameron.Models.Register> Register { get; set; }
+    }
+}
---
+++ Lab13Cameron/Lab13Cameron/Models/Register.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab13Cameron.Models
+{
+    public class Register
+    {
+        public int ID { get; set; }
+        public string MC { get; set; }
+        public string Hometown { get; set; }
+        public int Albums { get; set; }
+        [Display(Name = "1st Album")]
+        [DataType(DataType.Date)]
+        public DateTime FirstAlbum { get; set; }
+    }
+}
